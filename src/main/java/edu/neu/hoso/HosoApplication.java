bf21a655c9c4640dfcff71d57f64f87868754b17
Mike4Ellis <1007062267@qq.com>
2019-06-09 00:48:44 +0800

    first commit for test

---
+++ src/main/java/edu/neu/hoso/HosoApplication.java
@@ -0,0 +1,13 @@
+package edu.neu.hoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosoApplication.class, args);
+    }
+
+}
